fe1273489132e26f30af11b00d6d432947b25436
WangodaFrancis667 <fwangoda@gmail.com>
2025-08-14 09:49:54 +0300

    Added the CRF Exemption for services starting with /api/

--- main/settings.py
+++ main/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'accounts.middleware.CSRFExemptAPIMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
