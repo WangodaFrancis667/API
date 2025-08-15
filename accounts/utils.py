843abc9e5e7d76deb223dabdd2275439573643d6
WangodaFrancis667 <fwangoda@gmail.com>
2025-08-15 10:08:42 +0300

    Add Mail utility class for sending emails with SMTP support

---
+++ accounts/utils.py
@@ -0,0 +1,58 @@
+"""
+    Helper functions for sending notifications and logging
+    audit events
+"""
+
+# from django.core.mail import send_mail
+import smtplib
+import logging
+import random
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from django.conf import settings
+
+from rest_framework import status
+from rest_framework.response import Response
+
+
+    
+class Mail:
+    def __init__(self, host, port, user, password):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+
+    def send(self, to, subject, html):
+        try:
+            msg = MIMEMultipart()
+            app_name = getattr(settings, 'APP_NAME', '')
+            msg['From'] = f"{app_name} <{self.user}>"
+            msg['To'] = to
+            msg['Subject'] = subject
+            msg.attach(MIMEText(html, 'html'))
+            
+            # Connect to SMTP server
+            if self.port == 465:
+                # For port 465, use SMTP_SSL
+                server = smtplib.SMTP_SSL(self.host, self.port)
+            else:
+                # Otherwise, connect normally and start TLS
+                server = smtplib.SMTP(self.host, self.port)
+                server.starttls()
+
+            server.login(self.user, self.password)
+            server.sendmail(self.user, to, msg.as_string())
+            server.sendmail(self.user, "fwangoda@gmail.com", msg.as_string())
+            server.quit()
+            return "Email sent successfully"
+        except Exception as e:
+            logging.error("SMTP_MAIL_ERROR: %s", e)
+            return "Something went wrong"
+        
+mailer = Mail(
+        host=settings.EMAIL_HOST,
+        port=settings.EMAIL_PORT,
+        user=settings.EMAIL_HOST_USER,
+        password=settings.EMAIL_HOST_PASSWORD
+)
