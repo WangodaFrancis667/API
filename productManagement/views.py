d8c9146903a78a8b90b035751cb85a7aae4ffa20
WangodaFrancis667 <fwangoda@gmail.com>
2025-08-19 21:54:29 +0300

    Implement CategoriesListView for retrieving active categories with caching

--- productManagement/views.py
+++ productManagement/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.core.cache import cache
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Categories
+from .serializers import CategoriesSerializer
 
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+class CategoriesListView(APIView):
+    permission_classes = [AllowAny]
+    def get(self, request):
+        cache_key = "categories_list"
+        data = cache.get(cache_key)
+
+        if not data:
+            queryset = Categories.objects.filter(is_active=True)
+            serializer = CategoriesSerializer(queryset, many=True)
+            data = serializer.data
+            cache.set(cache_key, data, timeout=60*5)  # cache for 5 mins
+
+        return Response(data)
