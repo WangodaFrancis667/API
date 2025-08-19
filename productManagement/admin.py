9a1071f9b1bc945034e5c790d28dcb4849730e33
WangodaFrancis667 <fwangoda@gmail.com>
2025-08-19 21:54:06 +0300

    Implement CategoriesAdmin for managing Categories in the admin panel

--- productManagement/admin.py
+++ productManagement/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Categories
 
-# Register your models here.
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "is_active", "image_preview", "created_at", "updated_at")
+    search_fields = ("name",)
+    list_filter = ("is_active", "created_at")
+
+    def image_preview(self, obj):
+        if obj.image_url:
+            return format_html('<img src="https://afrobuyug.com/{}" width="60" height="60" />', obj.image_url)
+        return "-"
+    image_preview.short_description = "Image"
