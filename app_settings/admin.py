8e827bfe624173f9f663ada2fc39a98667c1f24e
WangodaFrancis667 <fwangoda@gmail.com>
2025-08-20 19:24:37 +0300

    Add admin interface for AppSettings: Register AppSettings model with custom admin options for better management

---
+++ app_settings/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import AppSettings
+
+# Register your models here.
+@admin.register(AppSettings)
+class AppSettingsAdmin(admin.ModelAdmin):
+    list_display = ("id", "setting_key", "setting_value", "created_at", "updated_at")
+    search_fields = ("setting_key",)
+    list_filter = ("setting_value", "created_at")
+
+   
