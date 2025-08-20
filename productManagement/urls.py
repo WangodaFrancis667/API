61127fccb8255819b39b671820c1fe2d0c2a2f49
WangodaFrancis667 <fwangoda@gmail.com>
2025-08-20 14:19:49 +0300

    Refactor URL patterns for product management views to improve organization and clarity

--- productManagement/urls.py
+++ productManagement/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import CategoriesListView
+from .views import CategoriesListView, ProductView, ProductFullView, ProductDetailView, ProductMetaDataView
 
 urlpatterns = [
-    path('view-categories/', CategoriesListView.as_view(), name='view-categories'),
+    path('categories/', CategoriesListView.as_view(), name='view-categories'),
+    path('products/', ProductFullView.as_view(), name='view-products'),
+
+    path('product-details/', ProductDetailView.as_view(), name='product-details'),
+    path('product-list/', ProductView.as_view(), name='product-list'),
+
+    path('product-metadata/', ProductMetaDataView.as_view(), name='product-metadata'),
 ]
