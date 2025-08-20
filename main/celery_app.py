6ed18487f6efb661c7cb597ec77a382a79202d69
WangodaFrancis667 <fwangoda@gmail.com>
2025-08-20 14:19:10 +0300

    Add Celery beat schedule for warming up product cache every 5 minutes

--- main/celery_app.py
+++ main/celery_app.py
@@ -8,5 +8,12 @@ app = Celery('main')
 # Load settingd from Django config with CELERY_ prefix
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+app.conf.beat_schedule = {
+    "warmup-products-cache-every-5-min": {
+        "task": "yourapp.tasks.warmup_product_cache",
+        "schedule": 300.0,  # every 5 minutes
+    },
+}
+
 # Discover tasks.py in all installed apps
 app.autodiscover_tasks()
