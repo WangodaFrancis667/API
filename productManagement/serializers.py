5ee8e8a5588350293e3b41265f3f9ce43dc515f5
WangodaFrancis667 <fwangoda@gmail.com>
2025-08-20 14:19:35 +0300

    Add serializers for Products, ProductImage, and ProductMetaData models

--- productManagement/serializers.py
+++ productManagement/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.conf import settings
-from .models import Categories
+from .models import Categories, Products, ProductImage, ProductMetaData
 
 class CategoriesSerializer(serializers.ModelSerializer):
     image_url = serializers.SerializerMethodField()
@@ -14,3 +14,29 @@ class CategoriesSerializer(serializers.ModelSerializer):
         if obj.image_url:
             return f"{settings.MEDIA_URL}{obj.image_url}"
         return None
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = ["id", "image_url"]
+
+
+class ProductsSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Products
+        fields = [
+            "id", "vendor_id", "title", "description",
+            "regular_price", "group_price", "min_quantity",
+            "unit", "category", "created_by_admin",
+            "created_at", "updated_at", "images"
+        ]
+
+
+# Product meta data serializer
+class ProductMetaDataSerializer(serializers.Serializer):
+    class Meta:
+        model = ProductMetaData
+        fields = "__all__"
