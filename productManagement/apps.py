48011e4b17a68245c21e2ae94ad6df4309bbf5c6
WangodaFrancis667 <fwangoda@gmail.com>
2025-08-20 14:19:20 +0300

    Add signals import in ProductmanagementConfig ready method

--- productManagement/apps.py
+++ productManagement/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ProductmanagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'productManagement'
+
+    def ready(self):
+        import productManagement.signals
