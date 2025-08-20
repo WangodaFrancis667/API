224d53da85c1a7b2183dd438209a5d354d9bb2ff
WangodaFrancis667 <fwangoda@gmail.com>
2025-08-20 17:23:01 +0300

    Refactor urls.py: Organize and enhance product management and metadata endpoints; add clear cache functionality

--- productManagement/urls.py
+++ productManagement/urls.py
@@ -1,12 +1,28 @@
 from django.urls import path
-from .views import CategoriesListView, ProductView, ProductFullView, ProductDetailView, ProductMetaDataView
+from .views import (
+    CategoriesListView, ClearMetadataCacheView ,ProductMetaDataByTypeView ,
+    ProductMetaDataDetailView ,ProductView, ProductFullView, 
+    ProductDetailView, ProductMetaDataListCreateView, ProductImageListCreateView,
+    ProductImageDetailView, BulkImageUploadView, ProductWithImagesView,
+)
 
 urlpatterns = [
     path('categories/', CategoriesListView.as_view(), name='view-categories'),
-    path('products/', ProductFullView.as_view(), name='view-products'),
 
-    path('product-details/', ProductDetailView.as_view(), name='product-details'),
+    # Product management endpoints
+    path('view-products/', ProductFullView.as_view(), name='view-products'),
+    path('product-details/<int:id>/', ProductDetailView.as_view(), name='product-details'),
     path('product-list/', ProductView.as_view(), name='product-list'),
 
-    path('product-metadata/', ProductMetaDataView.as_view(), name='product-metadata'),
+    # Product Image endpoints
+    path('<int:product_id>/images/', ProductImageListCreateView.as_view(), name='product-images-list-create'),
+    path('<int:product_id>/images/<int:id>/', ProductImageDetailView.as_view(), name='product-image-detail'),
+    path('images/bulk-upload/', BulkImageUploadView.as_view(), name='bulk-image-upload'),
+    path('<int:id>/with-images/', ProductWithImagesView.as_view(), name='product-with-images'),
+
+    # ProductMetaData endpoints
+    path('metadata/', ProductMetaDataListCreateView.as_view(), name='metadata-list-create'),
+    path('metadata/<int:pk>/', ProductMetaDataDetailView.as_view(), name='metadata-detail'),
+    path('metadata/type/<str:metadata_type>/', ProductMetaDataByTypeView.as_view(), name='metadata-by-type'),
+    path('metadata/clear-cache/', ClearMetadataCacheView.as_view(), name='clear-metadata-cache'),
 ]
