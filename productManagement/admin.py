4885004b745a40f1a7041729f2599b044abc30b3
WangodaFrancis667 <fwangoda@gmail.com>
2025-08-20 19:53:13 +0300

    Update admin display and filtering for product management: Enhance list_display and list_filter attributes for Categories, ProductMetaData, Products, and ProductImage admin classes to include created_by_name and improve search functionality.

--- productManagement/admin.py
+++ productManagement/admin.py
@@ -4,9 +4,9 @@ from .models import Categories, ProductMetaData, Products, ProductImage
 
 @admin.register(Categories)
 class CategoriesAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "is_active", "image_preview", "created_at", "updated_at")
+    list_display = ("id", "name", "is_active", "created_by_name", "image_preview", "created_at", "updated_at")
     search_fields = ("name",)
-    list_filter = ("is_active", "created_at")
+    list_filter = ("is_active", "created_by", "created_at")
 
     def image_preview(self, obj):
         if obj.image_url:
@@ -16,20 +16,22 @@ class CategoriesAdmin(admin.ModelAdmin):
 
 @admin.register(ProductMetaData)
 class ProductMetaDataAdmin(admin.ModelAdmin):
-    list_display = ("id", "type", "name", "display_name", "description", "category_type", "sort_order", "is_active", "created_at", "updated_at")
+    list_display = ("id", "type", "name", "display_name", "description", "category_type", "sort_order", "is_active", "created_by_name", "created_at", "updated_at")
     search_fields = ("name",)
-    list_filter = ("is_active", "created_at")
+    list_filter = ("is_active", "type", "created_by", "created_at")
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ("id", "vendor", "title", "description", "is_active", "regular_price", "group_price", "min_quantity", "unit", "category", "created_at", "updated_at"  )
-    search_fields = ("title",)
-    list_filter = ("is_active", "created_at")
+    list_display = ("id", "title", "category", "vendor_name", "created_by_name", "is_active", "regular_price", "group_price", "min_quantity", "unit", "created_at", "updated_at")
+    search_fields = ("title", "category__name", "vendor__username")
+    list_filter = ("is_active", "category", "vendor", "created_by", "created_at")
 
 @admin.register(ProductImage)
 class ProductImageAdmin(admin.ModelAdmin):
-    list_display = ("id", "product", "image_preview")
-    search_fields = ("product",)
+    list_display = ("id", "product", "created_by_name", "is_active", "image_preview", "created_at", "updated_at")
+    search_fields = ("product__title",)
+    list_filter = ("is_active", "created_by", "created_at")
+    
     def image_preview(self, obj):
         if obj.image_url:
             return format_html('<img src="https://api.afrobuyug.com/{}" width="60" height="60" />', obj.image_url)
