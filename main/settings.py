28a77c5c8a9f2fac1c4ed301e1db3ff4acf07ab9
Francis Wangoda <fwangoda@gmail.com>
2025-08-21 23:00:49 +0300

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -103,7 +103,7 @@ APP_NAME = 'AfroBuy'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASS"),
