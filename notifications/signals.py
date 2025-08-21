432343bccb390841a6463c31d85ea8d429162718
WangodaFrancis667 <fwangoda@gmail.com>
2025-08-21 11:41:15 +0300

    Refactor signal handlers to use the updated utility function for cache invalidation

--- notifications/signals.py
+++ notifications/signals.py
@@ -2,12 +2,12 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from .models import InAppNotifications
-from .services import _invalidate_user_cache
+from .utils import invalidate_user_cache
 
 @receiver(post_save, sender=InAppNotifications)
 def _notif_saved(sender, instance: InAppNotifications, **kwargs):
-    _invalidate_user_cache(instance.user_id, instance.user_type)
+    invalidate_user_cache(instance.user_id, instance.user_type)
 
 @receiver(post_delete, sender=InAppNotifications)
 def _notif_deleted(sender, instance: InAppNotifications, **kwargs):
-    _invalidate_user_cache(instance.user_id, instance.user_type)
+    invalidate_user_cache(instance.user_id, instance.user_type)
