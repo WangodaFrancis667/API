45abbce0b9c6f9120e0e504dae2207faed052145
WangodaFrancis667 <fwangoda@gmail.com>
2025-08-21 11:41:01 +0300

    Register InAppNotifications model in admin with custom display and filtering options

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,9 @@
+# notifications/admin.py
 from django.contrib import admin
+from .models import InAppNotifications
 
-# Register your models here.
+@admin.register(InAppNotifications)
+class InAppNotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'user_type', 'type', 'title', 'is_read', 'is_urgent', 'created_at', 'expires_at')
+    list_filter = ('user_type', 'type', 'is_read', 'is_urgent', 'created_at')
+    search_fields = ('title', 'message', 'otp_code', 'phone', 'user__email', 'user__username')
