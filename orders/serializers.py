e8359b6085a8ea874daacef7fb80cfd2ef24bd22
WangodaFrancis667 <fwangoda@gmail.com>
2025-09-01 09:00:50 +0300

    Refactor serializers for improved readability and maintainability

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,20 +3,22 @@ from .models import Order, OrderItem
 
 from rest_framework import serializers
 
+
 class UpdateOrderStatusSerializer(serializers.Serializer):
     order_id = serializers.IntegerField()
-    new_status = serializers.ChoiceField(choices=[
-        ('processing', 'processing'),
-        ('shipped', 'shipped'),
-        ('delivered', 'delivered'),
-        ('cancelled', 'cancelled'),
-    ])
+    new_status = serializers.ChoiceField(
+        choices=[
+            ("processing", "processing"),
+            ("shipped", "shipped"),
+            ("delivered", "delivered"),
+            ("cancelled", "cancelled"),
+        ]
+    )
+
 
 class OrderReturnCreateSerializer(serializers.Serializer):
     order_id = serializers.IntegerField()
     return_reason = serializers.CharField()
-
-
 
 
 class OrderItemCreateSerializer(serializers.Serializer):
@@ -26,17 +28,20 @@ class OrderItemCreateSerializer(serializers.Serializer):
 
 
 class OrderCreateSerializer(serializers.Serializer):
-    buyer_id = serializers.IntegerField(min_value=1)              # maps to AUTH_USER_MODEL id
+    buyer_id = serializers.IntegerField(min_value=1)  # maps to AUTH_USER_MODEL id
     vendor_id = serializers.IntegerField(min_value=1)
-    payment_method = serializers.ChoiceField(choices=['mobile_money','cash_on_delivery','bank_transfer'])
+    payment_method = serializers.ChoiceField(
+        choices=["mobile_money", "cash_on_delivery", "bank_transfer"]
+    )
     delivery_address = serializers.CharField(max_length=255)
     subtotal = serializers.DecimalField(max_digits=10, decimal_places=2)
     delivery_fee = serializers.DecimalField(max_digits=10, decimal_places=2)
     total_amount = serializers.DecimalField(max_digits=10, decimal_places=2)
     item = OrderItemCreateSerializer()
-    is_group_order = serializers.BooleanField(required=False, default=False)
+    # is_group_order = serializers.BooleanField(required=False, default=False)
+
 
 class OrderResponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ["id", "group_id", "total_amount", "payment_method", "status", "created_at"]
+        fields = ["id", "total_amount", "payment_method", "status", "created_at"]
