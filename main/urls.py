01473b0628e155192e761095c71bae63fda25e72
WangodaFrancis667 <fwangoda@gmail.com>
2025-09-01 21:47:24 +0300

    Refactor URL patterns for consistency and readability

--- main/urls.py
+++ main/urls.py
@@ -14,41 +14,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from APIHealth.views import health_check
 
 urlpatterns = [
-
     # Server root page view
-    path('', include('home_page.urls')),
-    
-    path('admin/', admin.site.urls),
-
+    path("", include("home_page.urls")),
+    path("admin/", admin.site.urls),
     # Health check
-    path('health/', health_check, name='health-check'),
-
+    path("health/", health_check, name="health-check"),
     # authentication
-    path('api/auth/', include('accounts.urls')),
-
+    path("api/auth/", include("accounts.urls")),
     # products
-    path('api/products/', include('productManagement.urls')),
-
+    path("api/products/", include("productManagement.urls")),
     # app settings
-    path('api/app/', include('app_settings.urls')),
-
+    path("api/app/", include("app_settings.urls")),
     # notifications
-    path('api/notifications/', include('notifications.urls')),
-
+    path("api/notifications/", include("notifications.urls")),
     # orders
     path("api/orders/", include("orders.urls")),
-    
     # App Versioning and force updates
     path("api/updates/", include("force_update.urls")),
-    
     # Earnings endpoints
     path("api/earnings/", include("earnings.urls")),
-
+    # payments endpoint
+    path("api/payments/", include("eversend_payments.urls")),
     # Add this line for DRF browsable API login/logout
     # path('api-auth/', include('rest_framework.urls')),
 ]
