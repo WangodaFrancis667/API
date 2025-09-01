58b4cbd61878be69ab1cae304af5a89021550bc1
WangodaFrancis667 <fwangoda@gmail.com>
2025-09-01 09:18:24 +0300

    Remove appVersions module and all associated files

--- appVersions/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppversionsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "appVersions"
--- appVersions/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import placeholder
-
-urlpatterns = [
-    path('test', placeholder, name='test')
-]
