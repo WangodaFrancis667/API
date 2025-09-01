761f3be137df9c216c4cc4fa2a1c6ce11b2b4f3b
WangodaFrancis667 <fwangoda@gmail.com>
2025-09-01 14:25:43 +0300

    Refactor argument handling in force update test command for clarity and consistency

--- force_update/management/commands/force_update_test.py
+++ force_update/management/commands/force_update_test.py
@@ -2,15 +2,24 @@ from django.core.management.base import BaseCommand
 from django.test import Client
 from django.urls import reverse
 
+
 class Command(BaseCommand):
     help = "Quickly test the force-update endpoint scenarios"
 
     def add_arguments(self, parser):
-        parser.add_argument("--force", action="store_true", help="Test force update scenario")
-        parser.add_argument("--optional", action="store_true", help="Test optional update scenario")
-        parser.add_argument("--none", action="store_true", help="Test no update scenario")
+        parser.add_argument(
+            "--force", action="store_true", help="Test force update scenario"
+        )
+        parser.add_argument(
+            "--optional", action="store_true", help="Test optional update scenario"
+        )
+        parser.add_argument(
+            "--none", action="store_true", help="Test no update scenario"
+        )
         parser.add_argument("--build", type=int, default=0, help="Custom build number")
-        parser.add_argument("--version", type=str, default="", help="Custom version string")
+        parser.add_argument(
+            "--app-version", type=str, default="", help="Custom version string"
+        )
 
     def handle(self, *args, **options):
         client = Client()
@@ -23,8 +32,8 @@ class Command(BaseCommand):
             params["test_no_update"] = "true"
         if options["build"]:
             params["current_build"] = str(options["build"])
-        if options["version"]:
-            params["current_version"] = options["version"]
+        if options["app_version"]:
+            params["current_version"] = options["app_version"]
 
         url = reverse("force_update")
         response = client.get(url, params)
